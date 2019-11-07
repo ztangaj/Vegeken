eaa003f0f81562b387036396979f8f0916a6a3c3
Junyan Zeng <jzengag@gmail.com>
2019-11-07 21:17:16 +0800

    zjy

--- app/src/androidTest/java/com/example/vegeken/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/vegeken/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("com.example.vegeken", appContext.getPackageName());//shshhehe
+        assertEquals("com.example.vegeken", appContext.getPackageName());//shshhe
     }
 }
